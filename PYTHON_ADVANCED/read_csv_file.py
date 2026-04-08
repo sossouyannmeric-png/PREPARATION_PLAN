d7534c780906856ef2362bfd5a57f05836e9eda7
SOSSOU Yann-Méric <yann-meric.sossou@epitech.eu>
2026-04-08 15:10:54 +0100

    Replace_the_folder_name_DATASET_REEL_by_REAL_DATASET

--- PYTHON_ADVANCED/read_csv_file.py
+++ PYTHON_ADVANCED/read_csv_file.py
@@ -1,7 +1,6 @@
-import pandas as pd #pandas library is useful for reading inside a csv file and create a dataframe(df) from it.
+import pandas as pd#Pandas is used to read and manipulate data from CSV files
 
-def read_csv_file():
+def read_csv_file():#This function reads a CSV file and returns a DataFrame
     df = pd.read_csv("mini_projet_sols_csv.csv")
-    print(df)
-    return (df)
-
+    print(df)#Display the content of the DataFrame (for checking)
+    return df
